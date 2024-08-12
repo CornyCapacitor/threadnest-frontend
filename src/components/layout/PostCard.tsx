b3e9b3b6143edd59afae97f43fb66e37b8439bfd
Mateusz Minder <mateusz.minder99@gmail.com>
2024-08-12 22:31:13 +0200

    upvoteButton.tsx finished

--- src/components/layout/PostCard.tsx
+++ src/components/layout/PostCard.tsx
@@ -1,4 +1,5 @@
 import { Post } from '@/app/types/postType'
+import Image from 'next/image'
 import { CommentButton } from './CommentButton'
 import { UpvoteButton } from './UpvoteButton'
 
@@ -8,10 +9,13 @@ type PostCardProps = {
 
 const PostCard = ({ post }: PostCardProps) => {
   return (
-    <div className="flex flex-col border-white border w-full p-2 gap-2">
-      <p>{post.author_username}</p>
-      <p>{post.title}</p>
-      <p>{post.content}</p>
+    <div className="min-w-[350px] flex flex-col p-5 pb-2 border-solid border-slate-400 border rounded-lg w-full gap-2">
+      <div className="flex gap-2 items-center justify-start pb-2 border-b border-slate-400 border-solid">
+        <Image src="/profile.svg" alt="Profile picture" width={30} height={30} className="bg-white rounded-full" />
+        <p>{post.author_username}</p>
+      </div>
+      <p className="border-b border-slate-400 border-solid pb-2">{post.title}</p>
+      <p className="border-b border-slate-400 border-solid pb-2">{post.content}</p>
       <div className="flex gap-2">
         <UpvoteButton postId={post._id} isUpvoted={post.upvoted} count={post.upvotesCount} />
         <CommentButton postId={post._id} count={post.commentsCount} />
--- src/components/layout/UpvoteButton.tsx
+++ src/components/layout/UpvoteButton.tsx
@@ -1,15 +1,75 @@
+import { userAtom } from '@/atoms/userAtom'
+import { isTokenExpired } from '@/utils/isTokenExpired'
+import { useAtom } from 'jotai'
 import Image from 'next/image'
+import { useState } from 'react'
 import { Button } from '../ui/button'
 
-const handleUpvote = async (postId: string) => {
-  console.log(`Upvoting/devoting post with id: ${postId}`)
-}
 
 export const UpvoteButton = ({ count, isUpvoted, postId }: { count: number, isUpvoted: boolean, postId: string }) => {
+  const [user, setUser] = useAtom(userAtom)
+  const [upvoted, setUpvoted] = useState(isUpvoted)
+  const [upvotesCount, setUpvotesCount] = useState(count)
+
+  const handleUpvote = async (postId: string) => {
+    console.log(`Upvoting/devoting post with id: ${postId}`)
+    if (!user) return
+
+    try {
+      const { token } = user
+
+      if (token && isTokenExpired(token)) {
+        console.log('Token has expired, you should re-login')
+        setUser(null)
+        return
+      }
+
+      // Optimistic update
+      setUpvoted(!upvoted);
+      setUpvotesCount(prevCount => upvoted ? prevCount - 1 : prevCount + 1);
+
+      const headers: HeadersInit = {
+        'Content-Type': 'application/json'
+      }
+
+      if (token) {
+        headers['Authorization'] = `Bearer ${token}`
+      }
+
+      const response = await fetch(`https://threadnest-backend.onrender.com/api/posts/${postId}?action=upvote`, {
+        method: 'PATCH',
+        headers: headers
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        // Realistic update
+        setUpvoted(data.upvoted)
+        setUpvotesCount(data.upvotesCount)
+        console.log('Updated succesfully')
+
+      } else {
+        console.error('Fetch failed with status:', response.status)
+        const errorData = await response.json()
+        console.error('Error data:', errorData)
+
+        // Turning back the optimistic update
+        setUpvoted(isUpvoted);
+        setUpvotesCount(count);
+
+        if (errorData.error === 'TokenExpiredError: jwt expired') {
+          console.log('You should relogin')
+        }
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <Button onClick={() => handleUpvote(postId)} className={`flex items-center justify-center w-16 ${isUpvoted ? 'bg-blue-400 hover:bg-blue-500' : ''}`}>
+    <Button onClick={() => handleUpvote(postId)} className={`flex items-center justify-center w-16 ${upvoted ? 'bg-blue-400 hover:bg-blue-500' : ''}`}>
       <Image src="/upvote.svg" alt="Upvote icon" width={16} height={16} />
-      <span className="font-semibold mt-1">{count}</span>
+      <span className="font-semibold mt-1">{upvotesCount}</span>
     </Button>
   )
 }
