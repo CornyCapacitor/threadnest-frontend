35f5f11da869e3beb8d619fa72dc1f60091c1653
Mateusz Minder <mateusz.minder99@gmail.com>
2024-09-13 22:38:58 +0200

    fixed post comment button

--- src/components/layout/CommentButton.tsx
+++ src/components/layout/CommentButton.tsx
@@ -1,31 +1,27 @@
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import { Button } from '../ui/button'
 
 export const CommentButton = ({ count, postId, disabled }: { count: number, postId: string, disabled?: boolean }) => {
-  const router = useRouter()
-
-  const handleRedirect = (postId: string) => {
-    router.push(`/post/${postId}`)
-  }
 
   return (
-    <Button
-      onClick={() => handleRedirect(postId)}
-      className='flex items-center justify-center w-16 bg-orange-600 hover:bg-orange-700'
-      disabled={disabled ? true : false}
-    >
-      <Image
-        src="/comment.svg"
-        alt="Upvote icon"
-        width={16}
-        height={16}
-      />
-      <span
-        className="font-semibold mt-1 ml-[1px]"
+    <Link href={`/post/${postId}`}>
+      <Button
+        className='flex items-center justify-center w-16 bg-orange-600 hover:bg-orange-700'
+        disabled={disabled ? true : false}
       >
-        {count}
-      </span>
-    </Button>
+        <Image
+          src="/comment.svg"
+          alt="Upvote icon"
+          width={16}
+          height={16}
+        />
+        <span
+          className="font-semibold mt-1 ml-[1px]"
+        >
+          {count}
+        </span>
+      </Button>
+    </Link>
   )
 }
